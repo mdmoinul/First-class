e9485610f2d1162ac10ca3c7837f48d334ab244f
Md Moinul Hoque Kamol <46660059+mdmoinul@users.noreply.github.com>
2019-01-14 07:02:13 +0600

    Update unset.php

--- unset.php
+++ unset.php
@@ -19,30 +19,4 @@ We can destroy variable by using unset function.
 
 
 
- Here is an example.
-$my_var='plus2net';
-echo " before unset : ".$my_var;
-echo "<br>";
-unset($my_var);
-echo "after unset : " .$my_var;
-The output is here
-before unset : plus2net
-after unset  :
-Unset function will remove local variable only. For example it can remove only variable inside the function and it does not affect outside the function. Here is one example to understand how unset function work inside function.
-$my_var='plus2net';
-
-function unset_var()
-{
-global $my_var;
-echo "Before unset and inside function :".$my_var."<br>";
-unset($my_var);
-echo "after unset and inside function  :".$my_var."<br>";
-}
-echo "Outside function before using function : ".$my_var."<br>";
-unset_var();
-echo "Outside function after using function : ".$my_var."<br>";
-Here is the output
-Outside function before using function : plus2net
-Before unset and inside function :plus2net
-after unset and inside function :
-Outside function after using function : plus2net
+ 
